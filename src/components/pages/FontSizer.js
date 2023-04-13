0792b8aa529d34eca85d7ad202ff740a4c8a0b46
james <james@devpipeline.com>
2023-04-12 22:34:30 -0600

    added fontsizer functionality

--- src/components/pages/FontSizer.js
+++ src/components/pages/FontSizer.js
@@ -1,3 +1,26 @@
+import { useState } from "react"
+
 export default function FontSizer() {
-  
+  const [fSize, setFSize] = useState(50)
+  return (
+    <div className="FontSizer">
+      <div className="TextWrapper">
+        <p style={{"fontSize" : fSize}}>{fSize}px</p>
+      </div>
+
+      <div className="ButtonWrapper">
+        <button onClick={() => {
+          if (fSize !== 115) {
+            setFSize((i) => (i + 5))}
+          }}>Grow</button>
+
+        <button onClick={() => {
+          if(fSize !== 5) {
+            setFSize((i) => (i - 5))}
+            
+          }}>Shrink</button>
+
+      </div>
+  </div>
+    )
 }
